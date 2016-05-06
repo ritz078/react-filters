519c9187d1a99ff0f03de815b2983541f01e4c31
Ritesh Kumar <rkritesh078@gmail.com>
2016-05-06 22:28:21 +0530

    refactor(files): relocated files and removed example files in favour of storybook

---
+++ tests/Switch.spec.js
@@ -0,0 +1,10 @@
+import {test} from 'ava';
+import {mount} from 'enzyme';
+import sinon from 'sinon';
+
+import Switch from '../components/Switch/Switch';
+
+
+test('<Switch/>', t => {
+  t.ok(true);
+});
